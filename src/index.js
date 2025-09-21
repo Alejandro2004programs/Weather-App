e6edd63a7a905b092875244ffdb6c33d3c94c25c
Alejandro Alvarez <alejandroalvarez1011204@gmail.com>
2025-09-20 21:54:22 -0700

    input correctly parses location and sends alert if not correct format or not available. current temperature and current conditions for a location now displayed

--- src/index.js
+++ src/index.js
@@ -8,21 +8,63 @@ class weatherData {
     }
 } 
 
+function compareSentenceLength(firstSentence, secondSentence) {
+    let count1 = 0;
+    let count2 = 0;
+    for(let i = 0; i < firstSentence.length; i++) {
+        if(firstSentence[i] === " ") {
+            count1 += 1;
+        }
+    }
+    for(let j = 0; j < secondSentence.length; j++) {
+        if(secondSentence[j] === " ") {
+            count2 += 1;
+        }
+    }
+    if(count1 === count2) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function upperCaseFirstLetter(string) {
+    let array = string.split(" ");
+    let newString = "";
+    for(let i = 0; i < array.length; i++) {
+        let currentWord = array[i];
+        newString = newString + currentWord[0].toUpperCase() + currentWord.substring(1) + " ";
+    }
+    return newString;
+}
+
 async function fetchData(location) {
     try {
         const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + location + "?key=4YJ3YVP7FRVQQ2267QZFJ7LU5");
         const json = await response.json();
-        return json;
+        if(json && compareSentenceLength(location, await json.resolvedAddress)) {
+            return json;
+        }
+        else {
+            throw new Error("This location input should not be accepted");
+        }
     }
     catch {
-
+        alert("No data available for this input");
     }
 }
 
 async function processData(promise) {
     const data = await promise;
-    const dataObject = await new weatherData(data.resolvedAddress, data.currentConditions, data.days);
+    console.log(data);
+    let location = data.resolvedAddress;
+    let upperCaseLocation = upperCaseFirstLetter(location);
+    const currentConditions = data.currentConditions;
+    const daysArray = data.days;
+    const dataObject = await new weatherData(upperCaseLocation, currentConditions, daysArray);
     console.log(dataObject);
+    return dataObject;
 }
 
 const locationForm = document.querySelector(".locationForm");
@@ -30,5 +72,18 @@ locationForm.addEventListener("submit", async(e) => {
     e.preventDefault();
     const inputData = locationForm.locationInput.value;
     const response = await fetchData(inputData);
-    processData(response);
+    if(response) {
+        const weatherObject = await processData(response);
+        updateDOM(weatherObject);
+    }
+    
 });
+
+async function updateDOM(weatherObject) {
+    const currentLocation = document.querySelector(".currentLocation");
+    const weatherConditions = document.querySelector(".weatherConditions");
+    const currentTemperature = document.querySelector(".currentTemperature");
+    currentLocation.textContent = await weatherObject.locationName;
+    weatherConditions.textContent = await weatherObject.currentConditions.conditions;
+    currentTemperature.textContent = Math.floor(await weatherObject.currentConditions.temp) + "°F";
+}
